13ac88ec10bfd1583f2e1d77e84a30de52bee809
Zeeshan-progs <md.zeeshaniqbal7277@gmail.com>
2022-06-04 02:18:21 +0530

    animated images (lottie)

--- lib/ui/dashboard/dashboard.dart
+++ lib/ui/dashboard/dashboard.dart
@@ -1,4 +1,8 @@
+import 'package:finance_friend/constants/assets.dart';
+import 'package:finance_friend/constants/color.dart';
+import 'package:finance_friend/utils/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -6,7 +10,24 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Dashnoard')),
+      backgroundColor: AppColor.white,
+      body: SafeArea(
+          child: Column(
+        children: [
+// lets add lottie image in our app
+DeviceUtils().blackHeight(20),
+          Container(
+            height: DeviceUtils.getScaledHeight(context, 0.4),
+            width: DeviceUtils.width(context),
+            child: Lottie.asset(
+              Assets.lottieImage,
+              repeat: true,
+              reverse: true,
+              frameRate: FrameRate.composition,
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
